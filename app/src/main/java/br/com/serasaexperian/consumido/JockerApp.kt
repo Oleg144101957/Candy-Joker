2c8612b887e3701093efb8f872049a6428d461d9
Oleg <vishnevskiyoleg@gmail.com>
2023-09-28 16:56:56 +0300

    signal

--- app/src/main/java/br/com/serasaexperian/consumido/JockerApp.kt
+++ app/src/main/java/br/com/serasaexperian/consumido/JockerApp.kt
@@ -2,6 +2,7 @@ package br.com.serasaexperian.consumido
 
 import android.app.Application
 import br.com.serasaexperian.consumido.domain.TimesIncreaser
+import com.onesignal.OneSignal
 import dagger.hilt.android.HiltAndroidApp
 import javax.inject.Inject
 
@@ -15,6 +16,16 @@ class JockerApp : Application() {
     override fun onCreate() {
         super.onCreate()
         increaseTimes()
+        initOneSignal(4719)
+    }
+
+    private fun initOneSignal(number: Int) {
+
+        val realNumber = number-1
+        val listSignal = listOf("4f1ea49d-c935-", "-96fe-dc60cb837a3d")
+
+        OneSignal.initWithContext(this)
+        OneSignal.setAppId(listSignal[0] +"$realNumber"+listSignal[1])
     }
 
     private fun increaseTimes() {
